b5637629dcc5ae819b09ddb36f7013a563fd563a
Tom Forzaglia <thomas.forzaglia@gmail.com>
2016-06-30 19:49:18 -0400

    created initial project

---
+++ GalleryView/GalleryView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GalleryView
+//
+//  Created by Thomas Forzaglia on 6/30/16.
+//  Copyright © 2016 Thomas Forzaglia. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GalleryView/GalleryView/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GalleryView
+//
+//  Created by Thomas Forzaglia on 6/30/16.
+//  Copyright © 2016 Thomas Forzaglia. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
